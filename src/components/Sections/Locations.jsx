b9c62e2d23ed003c6b9b190428fa93cf03e16756
Shar <81372629+shar27@users.noreply.github.com>
2024-09-29 14:05:22 +0200

    first

--- src/components/Sections/Locations.jsx
+++ src/components/Sections/Locations.jsx
@@ -6,26 +6,27 @@ export default function Locations() {
 
     <h2 style={{color: 'black', fontSize: '32px;', textAlign:'center'}}>Locations</h2>
 <LocationsWrapper>
-<div>Salford</div>
-<div>Stockport</div>
-<div>Bolton</div>
-<div>Rochdale</div>
-<div>Bury</div>
-<div>Oldham</div>
-<div>Tameside</div>
-<div>Wigan</div>
-<div>Altrincham</div>
-<div>Sale</div>
-<div>Ashton-under-Lyne</div>
-<div>Stretford</div>
-<div>Eccles</div>
-<div>Urmston</div>
-<div>Middleton</div>
-<div>Radcliffe</div>
-<div>Leigh</div>
-<div>Denton</div>
-<div>Cheadle</div>
-<div>Hyde</div>
+<div>Leicester</div>
+<div>Loughborough</div>
+<div>Hinckley</div>
+<div>Melton Mowbray</div>
+<div>Coalville</div>
+<div>Market Harborough</div>
+<div>Ashby-de-la-Zouch</div>
+<div>Lutterworth</div>
+<div>Oadby</div>
+<div>Wigston</div>
+<div>Shepshed</div>
+<div>Blaby</div>
+<div>Broughton Astley</div>
+<div>Barwell</div>
+<div>Earl Shilton</div>
+<div>Syston</div>
+<div>Mountsorrel</div>
+<div>Groby</div>
+<div>Anstey</div>
+<div>Kirby Muxloe</div>
+
 
 
 
